2c5b81c41a3ac6e53ce6258357db109d72a5c235
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-03-31 21:42:59 +1000

    Added diagnostic logging to test-coverage.ps1

--- test-coverage.ps1
+++ test-coverage.ps1
@@ -6,5 +6,5 @@ if (Test-Path -Path .\coverage-report) {
   Remove-Item .\coverage-report -Recurse -Force
 }
 
-dotnet test  --collect "XPlat Code Coverage" --results-directory ./coverage
+dotnet test --settings coverage.runsettings --results-directory ./coverage --diag ./coverage-logs
 reportgenerator -reports:"./coverage/**/coverage.cobertura.xml" -targetdir:"coverage-report" -reporttypes:"html"
