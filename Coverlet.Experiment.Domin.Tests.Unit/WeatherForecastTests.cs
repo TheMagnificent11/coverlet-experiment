d36d7a5e2386369eb14ed196fafef0dd7bb8ca06
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-03-31 16:56:36 +1000

    Weather Forecast Domain Class

---
+++ Coverlet.Experiment.Domin.Tests.Unit/WeatherForecastTests.cs
@@ -0,0 +1,26 @@
+﻿using Coverlet.Experiment.Domain;
+using FluentAssertions;
+using Xunit;
+
+namespace Coverlet.Experiment.Domin.Tests.Unit;
+
+public static class WeatherForecastTests
+{
+    [Theory]
+    [InlineData(-5, TemperatureSummary.Freezing)]
+    [InlineData(0, TemperatureSummary.Freezing)]
+    [InlineData(5, TemperatureSummary.Bracing)]
+    [InlineData(10, TemperatureSummary.Chilly)]
+    [InlineData(15, TemperatureSummary.Cool)]
+    [InlineData(20, TemperatureSummary.Mild)]
+    [InlineData(25, TemperatureSummary.Warm)]
+    [InlineData(30, TemperatureSummary.Balmy)]
+    [InlineData(35, TemperatureSummary.Hot)]
+    [InlineData(40, TemperatureSummary.Sweltering)]
+    [InlineData(45, TemperatureSummary.Scorching)]
+    public static void Summary(int temperatureC, TemperatureSummary expectedSummary)
+    {
+        var forecast = new WeatherForecast(DateOnly.FromDateTime(DateTime.Today), temperatureC);
+        forecast.Summary.Should().Be(expectedSummary);
+    }
+}
