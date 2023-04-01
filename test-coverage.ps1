a6455bcb80a9989414157fa080dbc5c3bbee442d
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-04-01 15:40:15 +1000

    coverage report

--- test-coverage.ps1
+++ test-coverage.ps1
@@ -1,10 +1,7 @@
-if (Test-Path -Path .\coverage) {
-  Remove-Item .\coverage -Recurse -Force
+
+if (Test-Path -Path .\coverage-*) {
+  Remove-Item .\coverage-* -Recurse -Force
 }
 
-if (Test-Path -Path .\coverage-report) {
-  Remove-Item .\coverage-report -Recurse -Force
-}
-
-dotnet test --settings coverage.runsettings --results-directory ./coverage --diag ./coverage-logs
-reportgenerator -reports:"./coverage/**/coverage.cobertura.xml" -targetdir:"coverage-report" -reporttypes:"html"
+dotnet test --settings coverage.runsettings --results-directory .\coverage-xml --diag .\coverage-logs\log.txt
+reportgenerator -reports:"./coverage-xml/**/coverage.cobertura.xml" -targetdir:"coverage-report" -reporttypes:"html"
