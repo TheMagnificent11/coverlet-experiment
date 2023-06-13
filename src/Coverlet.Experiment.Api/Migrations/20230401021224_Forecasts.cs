92e1d499105fec2085b4928d7a90554d4e9d1f3b
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-06-13 20:59:38 +1000

    Exclude EF migrations from Coverlet coverage

--- src/Coverlet.Experiment.Api/Migrations/20230401021224_Forecasts.cs
+++ src/Coverlet.Experiment.Api/Migrations/20230401021224_Forecasts.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Coverlet.Experiment.Api.Migrations;
 
 /// <inheritdoc />
+[ExcludeFromCodeCoverage]
 public partial class Forecasts : Migration
 {
     /// <inheritdoc />
